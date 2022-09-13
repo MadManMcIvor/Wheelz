55a980477507b13e2764399f8de0b1d75a7c443f
Julie Liao <jliao115@gmail.com>
2022-09-13 11:19:19 -0700

    sales model

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class AutomobileVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    vin = models.CharField(max_length=17, unique=True)
+
+class SalesPerson(models.Model):
+    name = models.CharField(max_length=200)
+    employee_number = models.PositiveSmallIntegerField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class SalesRecord(models.Model):
+    sales_person = models.ForeignKey(SalesPerson, related_name= "sales", on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, related_name="sales", on_delete=models.CASCADE)
+    automobile = models.ForeignKey(AutomobileVO, related_name="sales", on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+    is_sold = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.sales_person} {self.customer}"
