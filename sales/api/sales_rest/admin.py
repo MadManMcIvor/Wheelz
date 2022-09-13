347867e20a22118aeafd3771c0fc899b6e6395c8
Julie Liao <jliao115@gmail.com>
2022-09-13 11:26:27 -0700

    added admin

--- sales/api/sales_rest/admin.py
+++ sales/api/sales_rest/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from .models import AutomobileVO
+from .models import SalesPerson
+from .models import Customer
+from .models import SalesRecord
 
 
 # Register your models here.
 @admin.register(AutomobileVO)
 class AutomobileAdmin(admin.ModelAdmin):
     pass
+@admin.register(SalesPerson)
+class SalesPerson(admin.ModelAdmin):
+    pass
+
+@admin.register(Customer)
+class Customer(admin.ModelAdmin):
+    pass
+
+@admin.register(SalesRecord)
+class SalesRecord(admin.ModelAdmin):
+    pass 
