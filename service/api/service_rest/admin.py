cba75b56c392113d7b7c5a6f1ca52ad79c57e7fb
Alex <mcivor444@gmail.com>
2022-09-13 13:57:20 -0700

    Made the poller for service

--- service/api/service_rest/admin.py
+++ service/api/service_rest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import AutomobileVO
 
 # Register your models here.
+@admin.register(AutomobileVO)
+class AutomobileVOAdmin(admin.ModelAdmin):
+    pass
+
